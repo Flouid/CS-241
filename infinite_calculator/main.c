e351aff791cd86e92f985aaa6b00a84c349ebb15
Louis A. Keith - keithla <keithla@polaris.clarkson.edu>
2020-02-04 10:18:04 -0500

    Code explained during class.

---
+++ infinite_calculator/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <ctype.h>
+
+// Constants
+#define EOS		257
+#define NUM		258
+
+// Prototypes
+void error ( char * );
+int get_token() ;
+
+/* bounded memory calculator */
+int main()
+{
+	int current_token;
+
+	while ( current_token != EOS ) {
+		current_token = get_token();
+	}
+}
+
+int get_token()
+{
+	int c;
+	int value;
+
+	while (1) {
+		switch ( c = getchar() ) {
+		case '+': case '-': case '*': case '(': case ')':
+			fprintf( stderr, "[OP:%c]", c );
+			return c;	// return operators and brackets as is
+		case ' ': case '\t':
+			continue;	// ignore spaces and tabs
+		default:
+			if ( isdigit(c) ) {
+				fprintf( stderr, "[%c]", c );
+				while ( isdigit( c = getchar() )) {
+					fprintf( stderr, "[%c]", c );
+				}
+				ungetc( c, stdin );
+				return NUM;
+			}
+			else if (c == '\n') {
+				return EOS;
+			}
+			else {
+				fprintf( stderr, "{%c} ", c );
+				error( "Unknown token" );
+			}
+		}
+	}
+}
+
+void error( char *message )
+{
+	fprintf( stderr, "Error: %s\n", message );
+	exit(1);
+}
